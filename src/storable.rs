7d0a8927463be03c82323818a6d45712eff1d791
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2022-09-13 16:57:20 +0200

    Implement storable for unit

--- src/storable.rs
+++ src/storable.rs
@@ -27,6 +27,14 @@ pub trait Storable {
 // complexity of exposing these errors in the API of stable structures, an `unwrap`
 // in case of a detected error is preferable and safer.
 
+impl Storable for () {
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        std::borrow::Cow::Borrowed(&[])
+    }
+
+    fn from_bytes(_bytes: Vec<u8>) -> Self {}
+}
+
 impl Storable for Vec<u8> {
     fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
         std::borrow::Cow::Borrowed(self)
