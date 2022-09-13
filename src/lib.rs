e6641f7cf63472f7470c91a5a4cca7084da7c46c
Islam El-Ashi <ielashi@users.noreply.github.com>
2022-09-13 07:13:35 -0400

    chore: move `stable-structures` out of the  `ic` repo. (#4)
    Copies the updates to this crate as-is from the `ic` repo, with the
    following minor changes:
    
    1. Updating the crate name from `stable-structures` to
    `ic-stable-structures`.
    2. Updating the rust version in `rust-toolchain.toml`.
    3. derive `Eq` on a few structs to make clippy happy.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
-//pub mod hash_map;
+pub mod btreemap;
+pub mod cell;
 #[cfg(target_arch = "wasm32")]
-mod ic0_api;
+mod ic0_memory; // Memory API for canisters.
 pub mod log;
-#[cfg(test)]
-mod log_tests;
+pub mod memory_manager;
+pub mod storable;
 #[cfg(test)]
 mod tests;
+mod types;
 pub mod vec_mem;
 
-const WASM_PAGE_SIZE: u64 = 65536;
+pub use btreemap::StableBTreeMap;
+#[cfg(target_arch = "wasm32")]
+pub use ic0_memory::Ic0StableMemory;
+pub use storable::Storable;
+use types::Address;
+pub use vec_mem::VectorMemory;
+
+#[cfg(target_arch = "wasm32")]
+pub type DefaultMemoryImpl = Ic0StableMemory;
+
+#[cfg(not(target_arch = "wasm32"))]
+pub type DefaultMemoryImpl = VectorMemory;
+
+pub const WASM_PAGE_SIZE: u64 = 65536;
 
 pub trait Memory {
     /// Returns the current size of the stable memory in WebAssembly
@@ -29,17 +44,105 @@ pub trait Memory {
     fn write(&self, offset: u64, src: &[u8]);
 }
 
-/// A helper function that reads a single 32bit integer encoded as
-/// little-endian from the specified memory at the specified offset.
-fn read_u32<M: Memory>(m: &M, offset: u64) -> u32 {
+// A helper function that reads a single 32bit integer encoded as
+// little-endian from the specified memory at the specified offset.
+fn read_u32<M: Memory>(m: &M, addr: Address) -> u32 {
     let mut buf: [u8; 4] = [0; 4];
-    m.read(offset, &mut buf);
+    m.read(addr.get(), &mut buf);
     u32::from_le_bytes(buf)
+}
+
+// A helper function that reads a single 64bit integer encoded as
+// little-endian from the specified memory at the specified offset.
+fn read_u64<M: Memory>(m: &M, addr: Address) -> u64 {
+    let mut buf: [u8; 8] = [0; 8];
+    m.read(addr.get(), &mut buf);
+    u64::from_le_bytes(buf)
+}
+
+// Writes a single 32-bit integer encoded as little-endian.
+fn write_u32<M: Memory>(m: &M, addr: Address, val: u32) {
+    write(m, addr.get(), &val.to_le_bytes());
+}
+
+// Writes a single 64-bit integer encoded as little-endian.
+fn write_u64<M: Memory>(m: &M, addr: Address, val: u64) {
+    write(m, addr.get(), &val.to_le_bytes());
+}
+
+#[derive(Debug)]
+struct GrowFailed {
+    current_size: u64,
+    delta: u64,
+}
+
+/// Writes the bytes at the specified offset, growing the memory size if needed.
+fn safe_write<M: Memory>(memory: &M, offset: u64, bytes: &[u8]) -> Result<(), GrowFailed> {
+    let last_byte = offset
+        .checked_add(bytes.len() as u64)
+        .expect("Address space overflow");
+
+    let size_pages = memory.size();
+    let size_bytes = size_pages
+        .checked_mul(WASM_PAGE_SIZE)
+        .expect("Address space overflow");
+
+    if size_bytes < last_byte {
+        let diff_bytes = last_byte - size_bytes;
+        let diff_pages = diff_bytes
+            .checked_add(WASM_PAGE_SIZE - 1)
+            .expect("Address space overflow")
+            / WASM_PAGE_SIZE;
+        if memory.grow(diff_pages) == -1 {
+            return Err(GrowFailed {
+                current_size: size_pages,
+                delta: diff_pages,
+            });
+        }
+    }
+    memory.write(offset, bytes);
+    Ok(())
+}
+
+/// Like [safe_write], but panics if the memory.grow fails.
+fn write<M: Memory>(memory: &M, offset: u64, bytes: &[u8]) {
+    if let Err(GrowFailed {
+        current_size,
+        delta,
+    }) = safe_write(memory, offset, bytes)
+    {
+        panic!(
+            "Failed to grow memory from {} pages to {} pages (delta = {} pages).",
+            current_size,
+            current_size + delta,
+            delta
+        );
+    }
+}
+
+// Reads a struct from memory.
+fn read_struct<T, M: Memory>(addr: Address, memory: &M) -> T {
+    let mut t: T = unsafe { core::mem::zeroed() };
+    let t_slice = unsafe {
+        core::slice::from_raw_parts_mut(&mut t as *mut _ as *mut u8, core::mem::size_of::<T>())
+    };
+    memory.read(addr.get(), t_slice);
+    t
+}
+
+// Writes a struct to memory.
+fn write_struct<T, M: Memory>(t: &T, addr: Address, memory: &M) {
+    let slice = unsafe {
+        core::slice::from_raw_parts(t as *const _ as *const u8, core::mem::size_of::<T>())
+    };
+
+    write(memory, addr.get(), slice)
 }
 
 /// RestrictedMemory creates a limited view of another memory.  This
 /// allows one to divide the main memory into non-intersecting ranges
 /// and use different layouts in each region.
+#[derive(Clone)]
 pub struct RestrictedMemory<M: Memory> {
     page_range: core::ops::Range<u64>,
     memory: M,
--- src/log.rs
+++ src/log.rs
@@ -1,224 +1,375 @@
-use crate::{Memory, WASM_PAGE_SIZE};
-use std::cell::Cell;
+//! This module implements append-only list data structure, also known as log.
+//! It supports arbitrary-sized entries and dynamic sizing to arbitrary number of entries (as long as the underlying memory offers enough space).
+//! This requires two _independently growable_ Memory trait objects. For canister development it is recommended to use a [crate::memory_manager].
+//!
+//! # V1 layout
+//!
+//! This log uses two [crate::Memory] trait objects:
+//! * index memory to store the memory addresses of each entry
+//! * data memory to store the entries themselves
+//!
+//! ## Index memory
+//!
+//! ```text
+//! ---------------------------------------- <- Address 0
+//! Magic "GLI"             ↕ 3 bytes
+//! ----------------------------------------
+//! Layout version          ↕ 1 byte
+//! ----------------------------------------
+//! Reserved space          ↕ 28 bytes
+//! ---------------------------------------- <- Address 32 (HEADER_OFFSET)
+//! Number of entries = L   ↕ 8 bytes
+//! ---------------------------------------- <- Address 40
+//! E_0                     ↕ 8 bytes
+//! ----------------------------------------
+//! E_0 + E_1               ↕ 8 bytes
+//! ----------------------------------------
+//! ...
+//! ----------------------------------------
+//! E_0 + ... + E_(L-1)     ↕ 8 bytes
+//! ----------------------------------------
+//! Unused index entries    ↕ 8×(N-L) bytes
+//! ----------------------------------------
+//! Unallocated space
+//! ```
+//!
+//! ## Data memory
+//!
+//! ```text
+//! ---------------------------------------- <- Address 0
+//! Magic "GLD"             ↕ 3 bytes
+//! ----------------------------------------
+//! Layout version          ↕ 1 byte
+//! ----------------------------------------
+//! Reserved space          ↕ 28 bytes
+//! ---------------------------------------- <- Address 32 (HEADER_OFFSET)
+//! Entry 0 bytes           ↕ E_0 bytes
+//! ----------------------------------------
+//! Entry 1 bytes           ↕ E_1 bytes
+//! ----------------------------------------
+//! ...
+//! ----------------------------------------
+//! Entry (L-1) bytes       ↕ E_(L-1) bytes
+//! ----------------------------------------
+//! Unallocated space
+//! ```
+use crate::{read_u64, safe_write, write_u64, Address, GrowFailed, Memory};
 
-pub const LAYOUT_VERSION: u8 = 1;
+#[cfg(test)]
+mod tests;
 
-#[repr(packed)]
-struct Header {
+/// The magic number: Growable Log Index.
+pub const INDEX_MAGIC: &[u8; 3] = b"GLI";
+/// The magic number: Growable Log Data.
+pub const DATA_MAGIC: &[u8; 3] = b"GLD";
+
+/// The current version of the layout.
+const LAYOUT_VERSION: u8 = 1;
+
+/// The size of the V1 layout header.
+const HEADER_V1_SIZE: u64 = 4;
+
+/// The number of header bytes reserved for future extensions.
+const RESERVED_HEADER_SIZE: u64 = 28;
+
+/// Header offset to write data to.
+const HEADER_OFFSET: u64 = HEADER_V1_SIZE + RESERVED_HEADER_SIZE;
+
+struct HeaderV1 {
     magic: [u8; 3],
     version: u8,
-    max_entries: u32,
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum AllocError {
-    GrowFailed { current: u64, delta: u64 },
-    AddressSpaceOverflow,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum LoadError {
-    MemoryEmpty,
-    BadMagic([u8; 3]),
-    UnsupportedVersion(u8),
+pub enum InitError {
+    IncompatibleDataVersion {
+        last_supported_version: u8,
+        decoded_version: u8,
+    },
+    IncompatibleIndexVersion {
+        last_supported_version: u8,
+        decoded_version: u8,
+    },
+    InvalidIndex,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum WriteError {
-    IndexFull(u32),
-    GrowFailed { current: u64, delta: u64 },
-    AddressSpaceOverflow,
+    IndexFull { max_entries: u32 },
+    GrowFailed { current_size: u64, delta: u64 },
+}
+
+impl From<GrowFailed> for WriteError {
+    fn from(
+        GrowFailed {
+            current_size,
+            delta,
+        }: GrowFailed,
+    ) -> Self {
+        Self::GrowFailed {
+            current_size,
+            delta,
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct NoSuchEntry;
 
-pub struct StableLog<M: Memory> {
-    max_entries: u32,
-    num_entries: Cell<u32>,
-    index_offset: u32,
-    entries_offset: u32,
-    memory: M,
+/// Append-only list of variable-size entries stored in stable memory.
+pub struct Log<INDEX: Memory, DATA: Memory> {
+    index_memory: INDEX,
+    data_memory: DATA,
 }
 
-impl<M: Memory> StableLog<M> {
-    pub fn new(memory: M, max_entries: u32) -> Result<Self, AllocError> {
-        let header_len = core::mem::size_of::<Header>() as u32;
-
-        let index_len = max_entries
-            .checked_mul(4)
-            .ok_or(AllocError::AddressSpaceOverflow)?
-            .checked_add(4)
-            .ok_or(AllocError::AddressSpaceOverflow)?;
-
-        let header = Header {
-            magic: *b"LOG",
-            version: LAYOUT_VERSION,
-            max_entries,
+impl<INDEX: Memory, DATA: Memory> Log<INDEX, DATA> {
+    /// Creates a new empty growable stable log backed by the memory trait objects, overwriting the previous contents.
+    pub fn new(index_memory: INDEX, data_memory: DATA) -> Self {
+        let log = Self {
+            index_memory,
+            data_memory,
         };
+        Self::write_header(
+            &log.index_memory,
+            &HeaderV1 {
+                magic: *INDEX_MAGIC,
+                version: LAYOUT_VERSION,
+            },
+        );
+        Self::write_header(
+            &log.data_memory,
+            &HeaderV1 {
+                magic: *DATA_MAGIC,
+                version: LAYOUT_VERSION,
+            },
+        );
 
-        let header_slice = unsafe {
-            core::slice::from_raw_parts(
-                &header as *const _ as *const u8,
-                core::mem::size_of::<Header>(),
-            )
-        };
+        // number of entries
+        write_u64(&log.index_memory, Address::from(HEADER_OFFSET), 0);
+        log
+    }
 
-        if memory.size() < 1 && memory.grow(1) == -1 {
-            return Err(AllocError::GrowFailed {
-                current: 0,
-                delta: 1,
+    /// Initializes the log based on the contents of the provided memory trait objects.
+    /// If the memory trait objects already contain a stable log, this function recovers it from the stable
+    /// memory. Otherwise, this function allocates a new empty log.
+    pub fn init(index_memory: INDEX, data_memory: DATA) -> Result<Self, InitError> {
+        // if the data memory is not containing expected data, the index is useless anyway.
+        if data_memory.size() == 0 {
+            return Ok(Self::new(index_memory, data_memory));
+        }
+        let data_header = Self::read_header(&data_memory);
+        if &data_header.magic != DATA_MAGIC {
+            return Ok(Self::new(index_memory, data_memory));
+        }
+
+        if data_header.version != LAYOUT_VERSION {
+            return Err(InitError::IncompatibleDataVersion {
+                last_supported_version: LAYOUT_VERSION,
+                decoded_version: data_header.version,
             });
         }
-        memory.write(0, header_slice);
+
+        let index_header = Self::read_header(&index_memory);
+        if &index_header.magic != INDEX_MAGIC {
+            return Err(InitError::InvalidIndex);
+        }
+
+        if index_header.version != LAYOUT_VERSION {
+            return Err(InitError::IncompatibleIndexVersion {
+                last_supported_version: LAYOUT_VERSION,
+                decoded_version: index_header.version,
+            });
+        }
+
+        #[cfg(debug_assertions)]
+        {
+            assert_eq!(Ok(()), Self::validate_v1_index(&index_memory));
+        }
 
         Ok(Self {
-            max_entries,
-            num_entries: Cell::new(0),
-            index_offset: header_len,
-            entries_offset: header_len + index_len,
-            memory,
+            index_memory,
+            data_memory,
         })
     }
 
-    pub fn load(memory: M) -> Result<Self, LoadError> {
-        let mut header: Header = unsafe { core::mem::zeroed() };
-        let header_slice = unsafe {
-            core::slice::from_raw_parts_mut(
-                &mut header as *mut _ as *mut u8,
-                core::mem::size_of::<Header>(),
-            )
-        };
-        if memory.size() == 0 {
-            return Err(LoadError::MemoryEmpty);
+    /// Writes the stable log header to memory.
+    fn write_header(memory: &impl Memory, header: &HeaderV1) {
+        if memory.size() < 1 {
+            assert!(
+                memory.grow(1) != -1,
+                "failed to allocate the first memory page"
+            );
         }
-        memory.read(0, header_slice);
-        if &header.magic != b"LOG" {
-            return Err(LoadError::BadMagic(header.magic));
-        }
-        if header.version != LAYOUT_VERSION {
-            return Err(LoadError::UnsupportedVersion(header.version));
-        }
-        let index_offset = core::mem::size_of::<Header>() as u32;
-        let num_entries = crate::read_u32(&memory, index_offset as u64);
-        Ok(Self {
-            max_entries: header.max_entries,
-            num_entries: Cell::new(num_entries),
-            index_offset,
-            entries_offset: index_offset + header.max_entries * 4 + 4,
-            memory,
-        })
+        memory.write(0, &header.magic);
+        memory.write(3, &[header.version]);
     }
 
+    /// Reads the stable log header from the memory.
+    /// PRECONDITION: memory.size() > 0
+    fn read_header(memory: &impl Memory) -> HeaderV1 {
+        let mut magic = [0u8; 3];
+        let mut version = [0u8; 1];
+        memory.read(0, &mut magic);
+        memory.read(3, &mut version);
+        HeaderV1 {
+            magic,
+            version: version[0],
+        }
+    }
+
+    #[cfg(debug_assertions)]
+    fn validate_v1_index(memory: &INDEX) -> Result<(), String> {
+        let num_entries = read_u64(memory, Address::from(HEADER_OFFSET));
+
+        if num_entries == 0 {
+            return Ok(());
+        }
+
+        // Check that the index entries are non-decreasing.
+        let mut prev_entry = read_u64(memory, Address::from(HEADER_OFFSET + 8));
+        for i in 1..(num_entries as u64) {
+            let entry = read_u64(memory, Address::from(HEADER_OFFSET + 8 + i * 8));
+            if entry < prev_entry {
+                return Err(format!(
+                    "invalid entry I[{}]: {} < {}",
+                    i, entry, prev_entry
+                ));
+            }
+            prev_entry = entry;
+        }
+        Ok(())
+    }
+
+    /// Returns the underlying memory trait objects of the log.
+    pub fn forget(self) -> (INDEX, DATA) {
+        (self.index_memory, self.data_memory)
+    }
+
+    /// Returns true iff this log does not have any entries.
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
+    /// Returns the number of index memory bytes in use.
+    pub fn index_size_bytes(&self) -> usize {
+        let num_entries = read_u64(&self.index_memory, Address::from(HEADER_OFFSET));
+        self.index_entry_offset(num_entries).get() as usize
+    }
+
+    /// Returns the number of data memory bytes in use.
+    pub fn data_size_bytes(&self) -> usize {
+        self.log_size_bytes() + HEADER_OFFSET as usize
+    }
+
+    /// Returns the total size of all logged entries in bytes.
+    pub fn log_size_bytes(&self) -> usize {
+        let num_entries = self.len();
+        if num_entries == 0 {
+            0
+        } else {
+            read_u64(
+                &self.index_memory,
+                self.index_entry_offset((num_entries - 1) as u64),
+            ) as usize
+        }
+    }
+
+    /// Returns the number of entries in the log.
     pub fn len(&self) -> usize {
-        self.num_entries.get() as usize
+        read_u64(&self.index_memory, Address::from(HEADER_OFFSET)) as usize
     }
 
-    pub fn max_len(&self) -> usize {
-        self.max_entries as usize
-    }
-
-    pub fn get_entry(&self, idx: u32) -> Result<Vec<u8>, NoSuchEntry> {
+    /// Returns the entry at the specified index.
+    /// Returns None if the entry does not exist.
+    pub fn get(&self, idx: usize) -> Option<Vec<u8>> {
         let mut buf = vec![];
-        self.read_entry(idx, &mut buf)?;
-        Ok(buf)
+        self.read_entry(idx, &mut buf).ok().map(|_| buf)
     }
 
     /// Reads the contents of the entry with the specified index into
-    /// a byte vector.  The vector is resized to the entry size.
-    pub fn read_entry(&self, idx: u32, buf: &mut Vec<u8>) -> Result<(), NoSuchEntry> {
-        let (offset, len) = self.entry_meta(idx)?;
-        if buf.capacity() < len as usize {
-            buf.reserve(len as usize - buf.len());
-        }
-        // Safety: the check above verifies that the vector has enough
-        // capacity, and memory.read guarantees to fill the whole
-        // buffer.
-        unsafe {
-            buf.set_len(len as usize);
-        }
-        self.memory.read((self.entries_offset + offset) as u64, buf);
+    /// a byte vector.
+    ///
+    /// NOTE: if the entry exists, this function resizes `buf` to match the entry size.
+    ///
+    /// NOTE: this function returns a Result to make the compiler emit a warning if the caller
+    /// ignores the result.
+    pub fn read_entry(&self, idx: usize, buf: &mut Vec<u8>) -> Result<(), NoSuchEntry> {
+        let (offset, len) = self.entry_meta(idx).ok_or(NoSuchEntry)?;
+        buf.resize(len, 0);
+        self.data_memory.read((HEADER_OFFSET + offset) as u64, buf);
         Ok(())
     }
 
     /// Appends a new entry to the log.
     /// If successful, returns the index of the entry.
     ///
-    /// Postcondition: Ok(i) = log.append_entry(X) ⇒ log.get_entry(i) = X
-    pub fn append_entry(&self, bytes: &[u8]) -> Result<u32, WriteError> {
-        let n = self.num_entries.get();
-        if n == self.max_entries {
-            return Err(WriteError::IndexFull(n));
-        }
-
-        let offset = if n == 0 {
+    /// POST-CONDITION: Ok(idx) = log.append(E) ⇒ log.get(idx) = Some(E)
+    pub fn append(&self, bytes: &[u8]) -> Result<usize, WriteError> {
+        let idx = self.len() as u64;
+        let data_offset = if idx == 0 {
             0
         } else {
-            crate::read_u32(&self.memory, self.index_entry_offset(n - 1) as u64)
+            read_u64(&self.index_memory, self.index_entry_offset(idx - 1))
         };
 
-        let new_offset = offset
-            .checked_add(bytes.len() as u32)
-            .ok_or(WriteError::AddressSpaceOverflow)?;
+        let new_offset = data_offset
+            .checked_add(bytes.len() as u64)
+            .expect("address overflow");
 
-        let entry_offset = self
-            .entries_offset
-            .checked_add(offset)
-            .ok_or(WriteError::AddressSpaceOverflow)?;
+        let entry_offset = HEADER_OFFSET
+            .checked_add(data_offset)
+            .expect("address overflow");
 
-        self.write(entry_offset, bytes)?;
-        self.write(self.index_offset, &(n + 1).to_le_bytes())?;
-        self.write(self.index_entry_offset(n), &new_offset.to_le_bytes())?;
-        self.num_entries.set(n + 1);
-        Ok(n)
+        debug_assert!(new_offset >= data_offset);
+
+        // NB. we attempt to write the data first so we won't need to undo changes to the index if the write fails.
+        safe_write(&self.data_memory, entry_offset, bytes)?;
+
+        // NB. append to index first as it might need to grow the index memory.
+        safe_write(
+            &self.index_memory,
+            self.index_entry_offset(idx).get(),
+            &new_offset.to_le_bytes(),
+        )?;
+        // update number of entries
+        write_u64(
+            &self.index_memory,
+            Address::from(HEADER_OFFSET),
+            idx as u64 + 1,
+        );
+
+        debug_assert_eq!(self.get(idx as usize), Some(bytes.to_vec()));
+
+        Ok(idx as usize)
     }
 
-    pub fn entry_meta(&self, idx: u32) -> Result<(u32, u32), NoSuchEntry> {
-        if self.num_entries.get() <= idx {
-            return Err(NoSuchEntry);
+    /// Returns the offset and the length of the specified entry.
+    fn entry_meta(&self, idx: usize) -> Option<(u64, usize)> {
+        if self.len() <= idx {
+            return None;
         }
+
+        let idx = idx as u64;
         if idx == 0 {
-            Ok((
+            Some((
                 0,
-                crate::read_u32(&self.memory, self.index_entry_offset(0) as u64),
+                read_u64(&self.index_memory, self.index_entry_offset(0)) as usize,
             ))
         } else {
-            let offset = crate::read_u32(&self.memory, self.index_entry_offset(idx - 1) as u64);
-            let next = crate::read_u32(&self.memory, self.index_entry_offset(idx) as u64);
-            Ok((offset, next - offset))
+            let offset = read_u64(&self.index_memory, self.index_entry_offset(idx - 1));
+            let next = read_u64(&self.index_memory, self.index_entry_offset(idx));
+
+            debug_assert!(offset <= next);
+
+            Some((offset, (next - offset) as usize))
         }
     }
 
-    fn index_entry_offset(&self, idx: u32) -> u32 {
-        self.index_offset + idx * 4 + 4
-    }
-
-    fn write(&self, offset: u32, bytes: &[u8]) -> Result<(), WriteError> {
-        let last_byte = offset
-            .checked_add(bytes.len() as u32)
-            .ok_or(WriteError::AddressSpaceOverflow)?;
-        let size_pages = self.memory.size();
-        let size_bytes = size_pages
-            .checked_mul(WASM_PAGE_SIZE)
-            .ok_or(WriteError::AddressSpaceOverflow)?;
-        if size_bytes < last_byte as u64 {
-            let diff_bytes = last_byte as u64 - size_bytes;
-            let diff_pages = diff_bytes
-                .checked_add(WASM_PAGE_SIZE - 1)
-                .ok_or(WriteError::AddressSpaceOverflow)?
-                / WASM_PAGE_SIZE;
-            if self.memory.grow(diff_pages as u64) == -1 {
-                return Err(WriteError::GrowFailed {
-                    current: size_pages,
-                    delta: diff_pages,
-                });
-            }
-        }
-        self.memory.write(offset as u64, bytes);
-        Ok(())
+    /// Returns the absolute offset of the specified index entry in memory.
+    fn index_entry_offset(&self, idx: u64) -> Address {
+        Address::from(
+            HEADER_OFFSET + std::mem::size_of::<u64>() as u64 // skip over u64 storing the number of entries
+                + idx * (std::mem::size_of::<u64>() as u64), // memory addresses for idx many entries
+        )
     }
 }
