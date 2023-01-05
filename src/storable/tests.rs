b73c41f02b16191309edd6a5e292379d9cbb637a
Roman Kashitsyn <roman@dfinity.org>
2023-01-05 10:25:11 +0100

    typo
    Co-authored-by: Islam El-Ashi <islam.elashi@dfinity.org>

--- src/storable/tests.rs
+++ src/storable/tests.rs
@@ -13,7 +13,7 @@ proptest! {
     }
 
     #[test]
-    fn tuple_variable_width_rountrip(x in any::<u64>(), v in pvec(any::<u8>(), 0..40)) {
+    fn tuple_variable_width_roundtrip(x in any::<u64>(), v in pvec(any::<u8>(), 0..40)) {
         let bytes = Blob::<48>::try_from(&v[..]).unwrap();
         let tuple = (x, bytes);
         prop_assert_eq!(tuple, Storable::from_bytes(tuple.to_bytes().to_vec()));
