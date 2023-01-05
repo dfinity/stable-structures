cc69c0b2ee779bdc6e34c82d29eb382032e8d2d3
Islam El-Ashi <islam.elashi@dfinity.org>
2023-01-05 15:50:19 -0500

    .

--- benchmarks/src/main.rs
+++ benchmarks/src/main.rs
@@ -4,7 +4,7 @@ use tiny_rng::{Rand, Rng};
 /// Benchmarks inserting keys into a BTreeMap.
 #[ic_cdk_macros::query]
 pub fn btreemap_insert() -> u64 {
-    let mut btree: BTreeMap<_, u64, ()> = BTreeMap::init(DefaultMemoryImpl::default());
+    let mut btree: BTreeMap<u64, (), _> = BTreeMap::init(DefaultMemoryImpl::default());
 
     let num_keys = 10_000;
     let mut rng = Rng::from_seed(0);
@@ -24,7 +24,7 @@ pub fn btreemap_insert() -> u64 {
 /// Benchmarks removing keys from a BTreeMap.
 #[ic_cdk_macros::query]
 pub fn btreemap_remove() -> u64 {
-    let mut btree: BTreeMap<_, u64, ()> = BTreeMap::init(DefaultMemoryImpl::default());
+    let mut btree: BTreeMap<u64, (), _> = BTreeMap::init(DefaultMemoryImpl::default());
 
     let num_keys = 10_000;
     let mut rng = Rng::from_seed(0);
