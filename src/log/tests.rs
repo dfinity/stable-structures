4cb438819b9c8cc841f801570c0b563a63857e01
Roman Kashitsyn <roman@dfinity.org>
2023-01-09 15:55:26 +0100

    feat(log): make the stable log typed (#34)
    With this change, the Log type has an explicit Storable type argument
    instead of working with raw bytes.
    
    This interface is more convenient and consistent with other data
    structures.

--- src/log/tests.rs
+++ src/log/tests.rs
@@ -4,14 +4,14 @@ use crate::{Memory, RestrictedMemory, WASM_PAGE_SIZE};
 
 #[test]
 fn test_log_construct() {
-    let log = Log::new(VectorMemory::default(), VectorMemory::default());
+    let log = Log::<Vec<u8>, _, _>::new(VectorMemory::default(), VectorMemory::default());
 
     assert_eq!(log.len(), 0);
     assert_eq!(log.log_size_bytes(), 0);
     assert_eq!(log.index_size_bytes(), 40);
     let (index_memory, data_memory) = log.into_memories();
 
-    let log = Log::init(index_memory, data_memory).expect("failed to init log");
+    let log = Log::<Vec<u8>, _, _>::init(index_memory, data_memory).expect("failed to init log");
     assert_eq!(log.len(), 0);
     assert_eq!(log.log_size_bytes(), 0);
     assert_eq!(log.index_size_bytes(), 40);
@@ -19,20 +19,21 @@ fn test_log_construct() {
 
 #[test]
 fn test_new_overwrites() {
-    let log = Log::new(VectorMemory::default(), VectorMemory::default());
-    log.append(b"DEADBEEF").expect("failed to append entry");
+    let log = Log::<Vec<u8>, _, _>::new(VectorMemory::default(), VectorMemory::default());
+    log.append(&b"DEADBEEF".to_vec())
+        .expect("failed to append entry");
 
     assert_eq!(log.len(), 1);
 
     let (index_memory, data_memory) = log.into_memories();
-    let log = Log::new(index_memory, data_memory);
+    let log = Log::<Vec<u8>, _, _>::new(index_memory, data_memory);
     assert_eq!(log.len(), 0);
 }
 
 #[test]
 fn test_log_init_empty() {
-    let log =
-        Log::init(VectorMemory::default(), VectorMemory::default()).expect("failed to init log");
+    let log = Log::<Vec<u8>, _, _>::init(VectorMemory::default(), VectorMemory::default())
+        .expect("failed to init log");
 
     assert_eq!(log.len(), 0);
     assert_eq!(log.log_size_bytes(), 0);
@@ -44,7 +45,7 @@ fn test_log_init_with_different_data_magic() {
     let mem = VectorMemory::default();
     assert_eq!(mem.grow(1), 0);
     mem.write(0, b"WAS");
-    let log = Log::init(VectorMemory::default(), mem).expect("failed to init log");
+    let log = Log::<Vec<u8>, _, _>::init(VectorMemory::default(), mem).expect("failed to init log");
     assert_eq!(log.len(), 0);
 }
 
@@ -57,7 +58,9 @@ fn test_log_init_with_different_index_magic() {
     assert_eq!(data_mem.grow(1), 0);
     data_mem.write(0, b"GLD\x01");
     assert_eq!(
-        Log::init(index_mem, data_mem).map(|_| ()).unwrap_err(),
+        Log::<Vec<u8>, _, _>::init(index_mem, data_mem)
+            .map(|_| ())
+            .unwrap_err(),
         InitError::InvalidIndex
     );
 }
@@ -72,7 +75,7 @@ fn test_log_load_bad_index_version() {
     assert_eq!(data_memory.grow(1), 0);
     data_memory.write(0, b"GLD\x01");
     assert_eq!(
-        Log::init(index_memory, data_memory)
+        Log::<Vec<u8>, _, _>::init(index_memory, data_memory)
             .map(|_| ())
             .unwrap_err(),
         InitError::IncompatibleIndexVersion {
@@ -89,7 +92,7 @@ fn test_log_load_bad_data_version() {
     mem.write(0, b"GLD\x02");
 
     assert_eq!(
-        Log::init(VectorMemory::default(), mem)
+        Log::<Vec<u8>, _, _>::init(VectorMemory::default(), mem)
             .map(|_| ())
             .unwrap_err(),
         InitError::IncompatibleDataVersion {
@@ -101,9 +104,13 @@ fn test_log_load_bad_data_version() {
 
 #[test]
 fn test_log_append() {
-    let log = Log::new(VectorMemory::default(), VectorMemory::default());
-    let idx1 = log.append(b"DEADBEEF").expect("failed to append entry");
-    let idx2 = log.append(b"FEEDBAD").expect("failed to append entry");
+    let log = Log::<Vec<u8>, _, _>::new(VectorMemory::default(), VectorMemory::default());
+    let idx1 = log
+        .append(&b"DEADBEEF".to_vec())
+        .expect("failed to append entry");
+    let idx2 = log
+        .append(&b"FEEDBAD".to_vec())
+        .expect("failed to append entry");
 
     assert_eq!(idx1, 0);
     assert_eq!(idx2, 1);
@@ -114,12 +121,14 @@ fn test_log_append() {
 
 #[test]
 fn test_log_append_persistence() {
-    let log = Log::new(VectorMemory::default(), VectorMemory::default());
-    let idx = log.append(b"DEADBEEF").expect("failed to append entry");
+    let log = Log::<Vec<u8>, _, _>::new(VectorMemory::default(), VectorMemory::default());
+    let idx = log
+        .append(&b"DEADBEEF".to_vec())
+        .expect("failed to append entry");
 
     let (index_memory, data_memory) = log.into_memories();
 
-    let log = Log::init(index_memory, data_memory).unwrap();
+    let log = Log::<Vec<u8>, _, _>::init(index_memory, data_memory).unwrap();
     assert_eq!(log.len(), 1);
     assert_eq!(log.get(idx).unwrap(), b"DEADBEEF".to_vec());
     assert_eq!(log.log_size_bytes(), b"DEADBEEF".len());
@@ -131,48 +140,53 @@ fn test_log_append_persistence() {
 
 #[test]
 fn test_append_data_out_of_memory() {
-    let log = Log::new(
+    let log = Log::<Vec<u8>, _, _>::new(
         VectorMemory::default(),
         RestrictedMemory::new(VectorMemory::default(), 0..1),
     );
 
-    assert_eq!(Ok(0), log.append(b"small entry that fits into one page"));
-    assert_eq!(Ok(1), log.append(b"another small entry"));
+    assert_eq!(
+        Ok(0),
+        log.append(&b"small entry that fits into one page".to_vec())
+    );
+    assert_eq!(Ok(1), log.append(&b"another small entry".to_vec()));
     assert_eq!(
         Err(WriteError::GrowFailed {
             current_size: 1,
             delta: 1
         }),
-        log.append(&[1; WASM_PAGE_SIZE as usize])
+        log.append(&[1; WASM_PAGE_SIZE as usize].to_vec())
     );
     assert_eq!(2, log.len());
 }
 
 #[test]
 fn test_append_index_out_of_memory() {
-    let log = Log::new(
+    let log = Log::<Vec<u8>, _, _>::new(
         RestrictedMemory::new(VectorMemory::default(), 0..1),
         VectorMemory::default(),
     );
 
     for _ in 0..8_187 {
-        log.append(b"log").expect("failed to append entry");
+        log.append(&b"log".to_vec())
+            .expect("failed to append entry");
     }
     assert_eq!(
         Err(WriteError::GrowFailed {
             current_size: 1,
             delta: 1
         }),
-        log.append(b"log")
+        log.append(&b"log".to_vec())
     );
     assert_eq!(8_187, log.len());
 }
 
 #[test]
 fn test_index_grow() {
-    let log = Log::new(VectorMemory::default(), VectorMemory::default());
+    let log = Log::<Vec<u8>, _, _>::new(VectorMemory::default(), VectorMemory::default());
     for _ in 0..8_188 {
-        log.append(b"log").expect("failed to append entry");
+        log.append(&b"log".to_vec())
+            .expect("failed to append entry");
     }
     assert_eq!(log.index_size_bytes(), 65_544); // more than WASM_PAGE_SIZE
     let (index_memory, _) = log.into_memories();
