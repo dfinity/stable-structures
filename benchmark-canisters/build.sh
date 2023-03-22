6c43b61477c4b634293ff2c776c7dae509c012ae
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2023-03-22 17:21:34 +0100

    make common workspace

--- benchmark-canisters/build.sh
+++ benchmark-canisters/build.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 pushd "$SCRIPT_DIR"
 
 cargo build --release --target wasm32-unknown-unknown
-gzip -n -f ./target/wasm32-unknown-unknown/release/benchmarks.wasm
+gzip -n -f ../target/wasm32-unknown-unknown/release/benchmarks.wasm
