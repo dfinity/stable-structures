ae9fd851f92a3e354952fbbf4cfbd89d184465dc
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2023-03-22 17:10:19 +0100

    Add benchmarks with u64 values
    These changes add some benchmarks with `u64`s for the data in btreemaps
    and vecs. They also add a `Random` trait so that benchmarks for other
    types can easily be added.

--- benchmark-canisters/src/btreemap.rs
+++ benchmark-canisters/src/btreemap.rs
@@ -1,4 +1,4 @@
-use crate::count_instructions;
+use crate::{count_instructions, Random};
 use ic_cdk_macros::{query, update};
 use ic_stable_structures::{storable::Blob, BTreeMap, BoundedStorable, DefaultMemoryImpl};
 use tiny_rng::{Rand, Rng};
@@ -83,6 +83,21 @@ pub fn btreemap_insert_blob_1024_512() -> u64 {
     insert_blob_helper::<1024, 512>()
 }
 
+#[query]
+pub fn btreemap_insert_u64_u64() -> u64 {
+    insert_helper::<u64, u64>()
+}
+
+#[query]
+pub fn btreemap_insert_u64_blob_8() -> u64 {
+    insert_helper::<u64, Blob<8>>()
+}
+
+#[query]
+pub fn btreemap_insert_blob_8_u64() -> u64 {
+    insert_helper::<Blob<8>, u64>()
+}
+
 /// Benchmarks removing keys from a BTreeMap.
 #[update]
 pub fn btreemap_remove_blob_4_1024() -> u64 {
@@ -122,6 +137,21 @@ pub fn btreemap_remove_blob_256_1024() -> u64 {
 #[update]
 pub fn btreemap_remove_blob_512_1024() -> u64 {
     get_blob_helper::<512, 1024>()
+}
+
+#[update]
+pub fn btreemap_remove_u64_u64() -> u64 {
+    remove_helper::<u64, u64>()
+}
+
+#[update]
+pub fn btreemap_remove_u64_blob_8() -> u64 {
+    remove_helper::<u64, Blob<8>>()
+}
+
+#[update]
+pub fn btreemap_remove_blob_8_u64() -> u64 {
+    remove_helper::<Blob<8>, u64>()
 }
 
 /// Benchmarks getting keys from a BTreeMap.
@@ -165,50 +195,67 @@ pub fn btreemap_get_blob_512_1024() -> u64 {
     get_blob_helper::<512, 1024>()
 }
 
+#[query]
+pub fn btreemap_get_u64_u64() -> u64 {
+    get_helper::<u64, u64>()
+}
+
+#[query]
+pub fn btreemap_get_u64_blob_8() -> u64 {
+    get_helper::<u64, Blob<8>>()
+}
+
+#[query]
+pub fn btreemap_get_blob_8_u64() -> u64 {
+    get_helper::<Blob<8>, u64>()
+}
+
 // Profiles inserting a large number of random blobs into a btreemap.
 fn insert_blob_helper<const K: usize, const V: usize>() -> u64 {
-    let mut btree: BTreeMap<Blob<K>, Blob<V>, _> = BTreeMap::new(DefaultMemoryImpl::default());
+    insert_helper::<Blob<K>, Blob<V>>()
+}
+
+// Profiles inserting a large number of random blobs into a btreemap.
+fn insert_helper<K: Clone + Ord + BoundedStorable + Random, V: BoundedStorable + Random>() -> u64 {
+    let mut btree: BTreeMap<K, V, _> = BTreeMap::new(DefaultMemoryImpl::default());
     let num_keys = 10_000;
     let mut rng = Rng::from_seed(0);
     let mut random_keys = Vec::with_capacity(num_keys);
     let mut random_values = Vec::with_capacity(num_keys);
 
     for _ in 0..num_keys {
-        let key_size = rng.rand_u32() % Blob::<K>::MAX_SIZE;
-        let value_size = rng.rand_u32() % Blob::<V>::MAX_SIZE;
-        random_keys.push(random_vec(&mut rng, key_size));
-        random_values.push(random_vec(&mut rng, value_size));
+        random_keys.push(K::random(&mut rng));
+        random_values.push(V::random(&mut rng));
     }
 
     count_instructions(|| {
         // Insert the keys into the btree.
         for (k, v) in random_keys.into_iter().zip(random_values.into_iter()) {
-            btree.insert(
-                Blob::try_from(k.as_slice()).unwrap(),
-                Blob::try_from(v.as_slice()).unwrap(),
-            );
+            btree.insert(k, v);
         }
     })
 }
 
 // Profiles getting a large number of random blobs from a btreemap.
 fn get_blob_helper<const K: usize, const V: usize>() -> u64 {
-    let mut btree: BTreeMap<Blob<K>, Blob<V>, _> = BTreeMap::new(DefaultMemoryImpl::default());
+    get_helper::<Blob<K>, Blob<V>>()
+}
+
+fn get_helper<K: Clone + Ord + BoundedStorable + Random, V: BoundedStorable + Random>() -> u64 {
+    let mut btree: BTreeMap<K, V, _> = BTreeMap::new(DefaultMemoryImpl::default());
     let num_keys = 10_000;
     let mut rng = Rng::from_seed(0);
     let mut random_keys = Vec::with_capacity(num_keys);
     let mut random_values = Vec::with_capacity(num_keys);
 
     for _ in 0..num_keys {
-        let key_size = rng.rand_u32() % Blob::<K>::MAX_SIZE;
-        let value_size = rng.rand_u32() % Blob::<V>::MAX_SIZE;
-        random_keys.push(Blob::try_from(random_vec(&mut rng, key_size).as_slice()).unwrap());
-        random_values.push(Blob::try_from(random_vec(&mut rng, value_size).as_slice()).unwrap());
+        random_keys.push(K::random(&mut rng));
+        random_values.push(V::random(&mut rng));
     }
 
     // Insert the keys into the btree.
     for (k, v) in random_keys.iter().zip(random_values.into_iter()) {
-        btree.insert(*k, v);
+        btree.insert(k.clone(), v);
     }
 
     // Get all the keys from the map.
@@ -221,22 +268,24 @@ fn get_blob_helper<const K: usize, const V: usize>() -> u64 {
 
 // Inserts a large number of random blobs into a btreemap, then profiles removing them.
 fn remove_blob_helper<const K: usize, const V: usize>() -> u64 {
-    let mut btree: BTreeMap<Blob<K>, Blob<V>, _> = BTreeMap::new(DefaultMemoryImpl::default());
+    remove_helper::<Blob<K>, Blob<V>>()
+}
+
+fn remove_helper<K: Clone + Ord + BoundedStorable + Random, V: BoundedStorable + Random>() -> u64 {
+    let mut btree: BTreeMap<K, V, _> = BTreeMap::new(DefaultMemoryImpl::default());
     let num_keys = 10_000;
     let mut rng = Rng::from_seed(0);
     let mut random_keys = Vec::with_capacity(num_keys);
     let mut random_values = Vec::with_capacity(num_keys);
 
     for _ in 0..num_keys {
-        let key_size = rng.rand_u32() % Blob::<K>::MAX_SIZE;
-        let value_size = rng.rand_u32() % Blob::<V>::MAX_SIZE;
-        random_keys.push(Blob::try_from(random_vec(&mut rng, key_size).as_slice()).unwrap());
-        random_values.push(Blob::try_from(random_vec(&mut rng, value_size).as_slice()).unwrap());
+        random_keys.push(K::random(&mut rng));
+        random_values.push(V::random(&mut rng));
     }
 
     // Insert the keys into the btree.
     for (k, v) in random_keys.iter().zip(random_values.into_iter()) {
-        btree.insert(*k, v);
+        btree.insert(k.clone(), v);
     }
 
     count_instructions(|| {
@@ -245,8 +294,4 @@ fn remove_blob_helper<const K: usize, const V: usize>() -> u64 {
             btree.remove(&k);
         }
     })
-}
-
-fn random_vec(rng: &mut Rng, len: u32) -> Vec<u8> {
-    rng.iter(Rand::rand_u8).take(len as usize).collect()
 }
