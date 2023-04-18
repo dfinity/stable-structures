0c0b92ee231972d0a9bab7a12cd51ef941bd3e6e
0kenx <km@nxfi.app>
2023-04-18 22:30:57 +0200

    add tests

--- src/storable/tests.rs
+++ src/storable/tests.rs
@@ -35,4 +35,20 @@ proptest! {
     fn f32_roundtrip(v in any::<f32>()) {
         prop_assert_eq!(v, Storable::from_bytes(v.to_bytes()));
     }
+
+    #[test]
+    fn bool_roundtrip(v in any::<bool>()) {
+        prop_assert_eq!(v, Storable::from_bytes(v.to_bytes()));
+    }
+}
+
+#[cfg(feature = "candid")]
+#[test]
+fn should_work_principal_roundtrip() {
+    let v = Principal::from_text("aaaaa-aa").unwrap();
+    assert_eq!(v, Storable::from_bytes(v.to_bytes()));
+    let v = Principal::from_text("2chl6-4hpzw-vqaaa-aaaaa-c").unwrap();
+    assert_eq!(v, Storable::from_bytes(v.to_bytes()));
+    let v = Principal::anonymous();
+    assert_eq!(v, Storable::from_bytes(v.to_bytes()));
 }
