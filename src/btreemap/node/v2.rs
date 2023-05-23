d797134db71e5e2e3e528c5668e39e9461903580
Islam El-Ashi <islam.elashi@dfinity.org>
2023-05-23 13:11:20 +0200

    refactor save

--- src/btreemap/node/v2.rs
+++ src/btreemap/node/v2.rs
@@ -72,4 +72,91 @@ impl<K: Storable + Ord + Clone> Node<K> {
             version: header.version,
         }
     }
+
+    /// Saves the node to memory.
+    pub fn save_v2<M: Memory>(&mut self, memory: &M) {
+        let mut free_slots = self.get_free_slots();
+
+        let mut cell_array = vec![];
+
+        // Write the entries.
+        for (idx, key) in self.keys.iter().enumerate() {
+            // TODO: borrow outside?
+            if let Value::ByRef(slot_idx) = self.encoded_values.borrow()[idx] {
+                // Value hasn't been touched. Add idx to cell array and skip writing the keys and
+                // values.
+                cell_array.push(slot_idx);
+                continue;
+            }
+
+            // This is a new entry. Insert it at the next available free slot.
+
+            let free_slot_idx = free_slots.pop().unwrap();
+
+            cell_array.push(free_slot_idx);
+
+            let mut address = self.address
+                + Self::get_entry_offset(
+                    free_slot_idx as usize,
+                    self.max_key_size,
+                    self.max_value_size,
+                    self.version,
+                );
+
+            // Write the size of the key.
+            let key_bytes = key.to_bytes();
+            // TODO: asset key size.
+            write_u16(memory, address, key_bytes.len() as u16);
+            address += U16_SIZE;
+
+            // Write the key.
+            write(memory, address.get(), key_bytes.borrow());
+            address += Bytes::from(self.max_key_size);
+
+            // Write the size of the value.
+            let value = self.value(idx, memory);
+            write_u32(memory, address, value.len() as u32);
+            address += U32_SIZE;
+
+            // Write the value.
+            write(memory, address.get(), &value);
+            address += Bytes::from(self.max_value_size);
+        }
+
+        // Get the offset of the children. This line is a bit of a hack.
+        let address = self.address
+            + Self::get_entry_offset(
+                CAPACITY,
+                self.max_key_size,
+                self.max_value_size,
+                self.version,
+            );
+        let mut offset = Bytes::new(0);
+
+        // Write the children
+        for child in self.children.iter() {
+            write(memory, (address + offset).get(), &child.get().to_le_bytes());
+            offset += Address::size();
+        }
+
+        // Write the cell array.
+        cell_array.resize(CAPACITY, 0);
+        let cell_array: [u8; CAPACITY] = cell_array.try_into().unwrap();
+        write_struct(&cell_array, self.address + NodeHeader::size(), memory);
+
+        // Update the version (is this necessary?)
+        self.version = 2;
+
+        let header = NodeHeader {
+            magic: *MAGIC,
+            version: 2,
+            node_type: match self.node_type {
+                NodeType::Leaf => LEAF_NODE_TYPE,
+                NodeType::Internal => INTERNAL_NODE_TYPE,
+            },
+            num_entries: self.keys.len() as u16,
+        };
+
+        write_struct(&header, self.address, memory);
+    }
 }
