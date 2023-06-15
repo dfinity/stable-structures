023db67d6b35b9df696a0350b5a6e10ac9ab36f7
Roman Kashitsyn <roman.kashitsyn@gmail.com>
2023-06-15 12:57:08 +0200

    add tests

--- src/min_heap.rs
+++ src/min_heap.rs
@@ -1,8 +1,12 @@
 use crate::base_vec::{BaseVec, InitError};
 use crate::storable::BoundedStorable;
 use crate::{GrowFailed, Memory};
+use std::fmt;
 
-const MAGIC: [u8; 3] = *b"SHP"; // Short for "stable heap".
+#[cfg(test)]
+mod tests;
+
+const MAGIC: [u8; 3] = *b"SMH"; // Short for "stable min heap".
 
 /// An implementation of a binary heap.
 ///
@@ -10,6 +14,12 @@ const MAGIC: [u8; 3] = *b"SHP"; // Short for "stable heap".
 /// Motivation: max heaps are helpful for sorting, but most daily programming tasks require min
 /// heaps.
 pub struct MinHeap<T: BoundedStorable + PartialOrd, M: Memory>(BaseVec<T, M>);
+
+// Note: Heap Invariant
+// ~~~~~~~~~~~~~~~~~~~~
+//
+// HeapInvariant(heap, i, j) :=
+//   ∀ k: i ≤ k ≤ j: LET p = (k - 1)/2 IN (p ≤ i) => heap[p] ≤ heap[k]
 
 impl<T, M> MinHeap<T, M>
 where
@@ -46,8 +56,7 @@ where
     /// Pushes an item onto the min heap.
     pub fn push(&mut self, item: &T) -> Result<(), GrowFailed> {
         self.0.push(item)?;
-        debug_assert!(!self.is_empty());
-        self.bubble_up(self.0.len().saturating_sub(1), item);
+        self.bubble_up(self.0.len() - 1, item);
         debug_assert_eq!(Ok(()), self.check_invariant());
         Ok(())
     }
@@ -63,7 +72,7 @@ where
                 let smallest = self.0.get(0).unwrap();
                 let last = self.0.pop().unwrap();
                 self.0.set(0, &last);
-                self.bubble_down(0, n, &last);
+                self.bubble_down(0, n - 1, &last);
                 debug_assert_eq!(Ok(()), self.check_invariant());
                 Some(smallest)
             }
@@ -81,14 +90,19 @@ where
         self.0.iter()
     }
 
-    #[cfg(debug_assertions)]
+    pub fn into_memory(self) -> M {
+        self.0.into_memory()
+    }
+
+    #[allow(dead_code)]
+    /// Checks the HeapInvariant(self, 0, self.len() - 1)
     fn check_invariant(&self) -> Result<(), String> {
         let n = self.len();
-        for i in (1..n).rev() {
+        for i in 1..n {
             let p = (i - 1) / 2;
             let item = self.0.get(i).unwrap();
             let parent = self.0.get(p).unwrap();
-            if parent.partial_cmp(&item) == Some(std::cmp::Ordering::Greater) {
+            if is_less(&item, &parent) {
                 return Err(format!(
                     "Binary heap invariant violated in indices {i} and {p}"
                 ));
@@ -99,10 +113,11 @@ where
 
     fn bubble_up(&mut self, mut i: u64, item: &T) {
         // LOOP INVARIANT: self.0.get(i) == item
+        //                 ∧ HeapInvariant(self, i, self.len() - 1)
         while i > 0 {
             let p = (i - 1) / 2;
-            let parent = self.0.get(i).unwrap();
-            if item.partial_cmp(&parent) == Some(std::cmp::Ordering::Less) {
+            let parent = self.0.get(p).unwrap();
+            if is_less(item, &parent) {
                 self.0.set(i, &parent);
                 self.0.set(p, item);
             }
@@ -111,30 +126,65 @@ where
     }
 
     fn bubble_down(&mut self, mut i: u64, n: u64, item: &T) {
-        // LOOP INVARIANT: self.0.get(i) == item
+        // LOOP INVARIANT: ∧ self.0.get(i) == item
+        //                 ∧ HeapInvariant(self, 0, i)
         loop {
             let l = i * 2 + 1;
+            let r = l + 1;
+
             if n <= l {
                 return;
             }
-            let child = self.0.get(l).unwrap();
-            if child.partial_cmp(item) == Some(std::cmp::Ordering::Less) {
-                self.0.set(i, &child);
-                self.0.set(l, item);
-                i = l;
-                continue;
-            }
 
-            let r = l + 1;
             if n <= r {
-                return;
+                // Only the left child is withing the array bounds.
+
+                let child = self.0.get(l).unwrap();
+                if is_less(&child, item) {
+                    self.0.set(i, &child);
+                    self.0.set(l, item);
+                    i = l;
+                    continue;
+                }
+            } else {
+                // Both children are withing the array bounds.
+
+                let left = self.0.get(l).unwrap();
+                let right = self.0.get(r).unwrap();
+
+                if is_less(&left, item) {
+                    if is_less(&right, &left) {
+                        self.0.set(i, &right);
+                        self.0.set(r, item);
+                        i = r;
+                    } else {
+                        self.0.set(i, &left);
+                        self.0.set(l, item);
+                        i = l;
+                    }
+                    continue;
+                } else if is_less(&right, item) {
+                    self.0.set(i, &right);
+                    self.0.set(r, item);
+                    i = r;
+                    continue;
+                }
             }
-            let child = self.0.get(r).unwrap();
-            if child.partial_cmp(item) == Some(std::cmp::Ordering::Less) {
-                self.0.set(i, &child);
-                self.0.set(r, item);
-                i = r;
-            }
+            return;
         }
+    }
+}
+
+fn is_less<T: PartialOrd>(x: &T, y: &T) -> bool {
+    x.partial_cmp(y) == Some(std::cmp::Ordering::Less)
+}
+
+impl<T, M> fmt::Debug for MinHeap<T, M>
+where
+    T: BoundedStorable + PartialOrd + fmt::Debug,
+    M: Memory,
+{
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(fmt)
     }
 }
