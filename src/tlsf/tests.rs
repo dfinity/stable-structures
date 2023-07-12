5b074414604b039de2aec361268256e73d4f2cbb
Islam El-Ashi <islam.elashi@dfinity.org>
2023-07-12 07:22:39 +0200

    multiple allocations proptest

--- src/tlsf/tests.rs
+++ src/tlsf/tests.rs
@@ -58,3 +58,41 @@ fn deallocate_everything() {
         );
     });
 }
+
+#[test]
+fn multiple_allocations_no_deallocations() {
+    proptest!(|(
+        data in proptest::collection::vec(
+            proptest::collection::vec(0..u8::MAX, 3usize..100usize), 1..100
+        ),
+    )| {
+        let mem = make_memory();
+        let mut tlsf = TlsfAllocator::new(mem);
+        let mut addresses: Vec<(Address, Vec<u8>)> = vec![];
+
+        let mut offset = Bytes::new(0);
+        for d in data.into_iter() {
+            let address = tlsf.allocate(d.len() as u32);
+
+            // Asserts that the free lists have been updated accordingly.
+            offset += Bytes::from(Block::header_size()) + Bytes::from(d.len() as u64);
+            prop_assert_eq!(
+                tlsf.free_lists[FIRST_LEVEL_INDEX_SIZE - 1][SECOND_LEVEL_INDEX_SIZE - 1],
+                DATA_OFFSET + offset
+            );
+
+            // Write the data into memory.
+            tlsf.memory.write(address.get(), &d);
+            addresses.push((address, d));
+        }
+
+        // Read all the data from memory.
+        let mut v = Vec::new();
+        for (address, data) in addresses {
+            v.resize(data.len(), 0);
+            // Read data from memory and verify its there.
+            tlsf.memory.read(address.get(), &mut v);
+            prop_assert_eq!(&v, &data);
+        }
+    });
+}
