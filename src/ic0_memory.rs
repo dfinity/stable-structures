de5923e97aaf48a5946c4a2ba3c6298a865ebf74
Linwei Shang <linwei.shang@dfinity.org>
2023-07-21 12:43:26 -0400

    remove

--- src/ic0_memory.rs
+++ src/ic0_memory.rs
@@ -1,13 +1,5 @@
 use crate::Memory;
 
-// #[link(wasm_import_module = "ic0")]
-// extern "C" {
-//     pub fn stable64_size() -> u64;
-//     pub fn stable64_grow(additional_pages: u64) -> i64;
-//     pub fn stable64_read(dst: u64, offset: u64, size: u64);
-//     pub fn stable64_write(offset: u64, src: u64, size: u64);
-// }
-
 #[derive(Clone, Copy, Default)]
 pub struct Ic0StableMemory;
 
