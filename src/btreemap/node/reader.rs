4e221acda5e9e50e54751a45c9ba7867d713b351
Islam El-Ashi <islam.elashi@dfinity.org>
2023-08-30 10:12:43 +0200

    use segments with page idx

--- src/btreemap/node/reader.rs
+++ src/btreemap/node/reader.rs
@@ -26,21 +26,31 @@ impl<'a, M: Memory> Memory for NodeReader<'a, M> {
         }
 
         let iter = NodeIterator {
-            virtual_segment: Segment {
+            virtual_segment: VirtualSegment {
                 address: Address::from(offset),
                 length: Bytes::from(dst.len() as u64),
             },
-            address: self.address,
-            overflows: &self.overflows,
             page_size: Bytes::from(self.page_size.get()),
         };
 
         let mut bytes_read = 0;
-        for Segment { address, length } in iter {
-            self.memory.read(
-                address.get(),
-                &mut dst[bytes_read as usize..(bytes_read + length.get()) as usize],
-            );
+        for RealSegment {
+            page_idx,
+            offset,
+            length,
+        } in iter
+        {
+            if page_idx == 0 {
+                self.memory.read(
+                    (self.address + offset).get(),
+                    &mut dst[bytes_read as usize..(bytes_read + length.get()) as usize],
+                );
+            } else {
+                self.memory.read(
+                    (self.overflows[page_idx - 1] + offset).get(),
+                    &mut dst[bytes_read as usize..(bytes_read + length.get()) as usize],
+                );
+            }
 
             bytes_read += length.get();
         }
@@ -52,20 +62,24 @@ impl<'a, M: Memory> Memory for NodeReader<'a, M> {
 }
 
 #[derive(Debug)]
-struct Segment {
-    pub address: Address,
-    pub length: Bytes,
+struct VirtualSegment {
+    address: Address,
+    length: Bytes,
 }
 
-struct NodeIterator<'a> {
-    pub virtual_segment: Segment,
-    pub address: Address,
-    pub overflows: &'a [Address],
-    pub page_size: Bytes,
+struct RealSegment {
+    page_idx: usize,
+    offset: Bytes,
+    length: Bytes,
 }
 
-impl Iterator for NodeIterator<'_> {
-    type Item = Segment;
+struct NodeIterator {
+    virtual_segment: VirtualSegment,
+    page_size: Bytes,
+}
+
+impl Iterator for NodeIterator {
+    type Item = RealSegment;
 
     fn next(&mut self) -> Option<Self::Item> {
         // The segment is empty. End iteration.
@@ -76,9 +90,8 @@ impl Iterator for NodeIterator<'_> {
         // Map the virtual segment's address to a real address.
         let offset = Bytes::from(self.virtual_segment.address.get());
 
-        let (real_address, bytes_in_segment) = if offset < self.page_size {
+        let segment = if offset < self.page_size {
             // The address is in the initial page.
-            let real_address = self.address + offset;
 
             // Compute how many bytes are in this real segment.
             let bytes_in_segment = {
@@ -88,7 +101,11 @@ impl Iterator for NodeIterator<'_> {
                 min(end_page_offset - offset, self.virtual_segment.length)
             };
 
-            (real_address, bytes_in_segment)
+            RealSegment {
+                page_idx: 0,
+                offset,
+                length: bytes_in_segment,
+            }
         } else {
             // The amount of data that can be stored in an overflow page.
             let overflow_data_size = self.page_size - PAGE_OVERFLOW_DATA_OFFSET;
@@ -103,10 +120,6 @@ impl Iterator for NodeIterator<'_> {
             let overflow_page_end_offset =
                 self.page_size + Bytes::from((overflow_idx + 1) as u64) * overflow_data_size;
 
-            let real_address = self.overflows[overflow_idx]
-                + PAGE_OVERFLOW_DATA_OFFSET
-                + Bytes::new(offset_in_overflow as u64);
-
             // Compute how many bytes are in this real segment.
             let bytes_in_segment = {
                 let end_page_offset = overflow_page_end_offset;
@@ -115,16 +128,17 @@ impl Iterator for NodeIterator<'_> {
                 min(end_page_offset - offset, self.virtual_segment.length)
             };
 
-            (real_address, bytes_in_segment)
+            RealSegment {
+                page_idx: overflow_idx + 1,
+                offset: PAGE_OVERFLOW_DATA_OFFSET + Bytes::new(offset_in_overflow as u64),
+                length: bytes_in_segment,
+            }
         };
 
         // Update the virtual segment to exclude the portion we're about to return.
-        self.virtual_segment.length -= bytes_in_segment;
-        self.virtual_segment.address += bytes_in_segment;
+        self.virtual_segment.length -= segment.length;
+        self.virtual_segment.address += segment.length;
 
-        Some(Segment {
-            address: real_address,
-            length: bytes_in_segment,
-        })
+        Some(segment)
     }
 }
