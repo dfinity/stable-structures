532a47cd175014946bf9fee5f70b0160e42ced20
Andriy Berestovskyy <andriy.berestovskyy@dfinity.org>
2025-03-04 15:20:08 +0100

    Minor fixes

--- src/btreemap/node/v2.rs
+++ src/btreemap/node/v2.rs
@@ -111,7 +111,7 @@ impl<K: Storable + Ord + Clone> Node<K> {
         header: NodeHeader,
         memory: &M,
     ) -> Self {
-        #[cfg(feature = "canbench-rs")]
+        #[cfg(feature = "canbench")]
         let _p = canbench::profile("node_load_v2");
 
         // Load the node, including any overflows, into a buffer.
@@ -191,7 +191,7 @@ impl<K: Storable + Ord + Clone> Node<K> {
 
     // Saves the node to memory.
     pub(super) fn save_v2<M: Memory>(&mut self, allocator: &mut Allocator<M>) {
-        #[cfg(feature = "canbench-rs")]
+        #[cfg(feature = "canbench")]
         let _p = canbench::profile("node_save_v2");
 
         let page_size = self.version.page_size().get();
