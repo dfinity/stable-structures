dc2e2e387e57e63310d468b127755cac37180218
Dragoljub Duric <dragoljub.duric@dfinity.org>
2025-04-29 11:03:35 +0000

    Add new benchmarks

--- benchmarks/src/btreeset.rs
+++ benchmarks/src/btreeset.rs
@@ -4,6 +4,13 @@ use ic_stable_structures::{btreeset::BTreeSet, DefaultMemoryImpl, Storable};
 
 // Define type alias for Blob<8>.
 type Blob8 = Blob<8>;
+type Blob16 = Blob<16>;
+type Blob32 = Blob<32>;
+type Blob64 = Blob<64>;
+type Blob128 = Blob<128>;
+type Blob256 = Blob<256>;
+type Blob512 = Blob<512>;
+type Blob1024 = Blob<1024>;
 
 /// Helper macro to generate benchmarks.
 macro_rules! bench_tests {
@@ -71,6 +78,38 @@ fn range_helper<K: Clone + Ord + Storable>() -> BenchResult {
     bench_fn(|| for _ in btreeset.range(start..end) {})
 }
 
+// Profiles the union operation on two BTreeSets.
+fn union_helper<K: Clone + Ord + Storable>() -> BenchResult {
+    let mut btreeset1 = BTreeSet::new(DefaultMemoryImpl::default());
+    let mut btreeset2 = BTreeSet::new(DefaultMemoryImpl::default());
+    let num_keys = 1_000;
+
+    for i in 0..num_keys {
+        btreeset1.insert(generate_key::<K>(i));
+        if i % 2 == 0 {
+            btreeset2.insert(generate_key::<K>(i));
+        }
+    }
+
+    bench_fn(|| for _ in btreeset1.union(&btreeset2) {})
+}
+
+// Profiles the intersection operation on two BTreeSets.
+fn intersection_helper<K: Clone + Ord + Storable>() -> BenchResult {
+    let mut btreeset1 = BTreeSet::init(DefaultMemoryImpl::default());
+    let mut btreeset2 = BTreeSet::init(DefaultMemoryImpl::default());
+    let num_keys = 1_000;
+
+    for i in 0..num_keys {
+        btreeset1.insert(generate_key::<K>(i));
+        if i % 2 == 0 {
+            btreeset2.insert(generate_key::<K>(i));
+        }
+    }
+
+    bench_fn(|| for _ in btreeset1.intersection(&btreeset2) {})
+}
+
 // Generates keys directly based on the type `K`.
 fn generate_key<K: Storable>(i: u32) -> K {
     K::from_bytes(std::borrow::Cow::Owned(i.to_be_bytes().to_vec()))
@@ -86,4 +125,28 @@ bench_tests! {
     btreeset_iter_blob_8, iter_helper, Blob8;
     btreeset_range_u32, range_helper, u32;
     btreeset_range_blob_8, range_helper, Blob8;
+}
+
+// Add benchmarks for union and intersection with additional key types.
+bench_tests! {
+    btreeset_union_u32, union_helper, u32;
+    btreeset_union_u64, union_helper, u64;
+    btreeset_union_blob_8, union_helper, Blob8;
+    btreeset_union_blob_16, union_helper, Blob16;
+    btreeset_union_blob_32, union_helper, Blob32;
+    btreeset_union_blob_64, union_helper, Blob64;
+    btreeset_union_blob_128, union_helper, Blob128;
+    btreeset_union_blob_256, union_helper, Blob256;
+    btreeset_union_blob_512, union_helper, Blob512;
+    btreeset_union_blob_1024, union_helper, Blob1024;
+    btreeset_intersection_u32, intersection_helper, u32;
+    btreeset_intersection_u64, intersection_helper, u64;
+    btreeset_intersection_blob_8, intersection_helper, Blob8;
+    btreeset_intersection_blob_16, intersection_helper, Blob16;
+    btreeset_intersection_blob_32, intersection_helper, Blob32;
+    btreeset_intersection_blob_64, intersection_helper, Blob64;
+    btreeset_intersection_blob_128, intersection_helper, Blob128;
+    btreeset_intersection_blob_256, intersection_helper, Blob256;
+    btreeset_intersection_blob_512, intersection_helper, Blob512;
+    btreeset_intersection_blob_1024, intersection_helper, Blob1024;
 }
