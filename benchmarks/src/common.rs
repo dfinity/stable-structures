ddd308a8cf7a62a270d211455b7f55a50ed75503
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-05-27 15:07:54 +0200

    revert random

--- benchmarks/src/common.rs
+++ benchmarks/src/common.rs
@@ -6,39 +6,38 @@ pub trait Random {
     fn random(rng: &mut Rng) -> Self;
 }
 
-#[inline]
-fn random_bytes(rng: &mut Rng, max_size: u32) -> Vec<u8> {
-    let size = (rng.rand_u32() % (max_size + 1)) as usize;
-    if size == 0 {
-        Vec::new()
-    } else {
-        let mut buf = Vec::with_capacity(size);
-        buf.extend(rng.iter(Rand::rand_u8).take(size));
-        buf
-    }
-}
-
 impl<const K: usize> Random for Blob<K> {
-    #[inline]
     fn random(rng: &mut Rng) -> Self {
-        let bytes = random_bytes(rng, Blob::<K>::BOUND.max_size());
-        Blob::try_from(&bytes[..]).unwrap()
-    }
-}
-
-impl<const K: usize> Random for UnboundedVecN<K> {
-    #[inline]
-    fn random(rng: &mut Rng) -> Self {
-        let bytes = random_bytes(rng, Self::max_size());
-        Self::from(bytes)
+        let size = rng.rand_u32() % Blob::<K>::BOUND.max_size();
+        Blob::try_from(
+            rng.iter(Rand::rand_u8)
+                .take(size as usize)
+                .collect::<Vec<_>>()
+                .as_slice(),
+        )
+        .unwrap()
     }
 }
 
 impl<const K: usize> Random for BoundedVecN<K> {
-    #[inline]
     fn random(rng: &mut Rng) -> Self {
-        let bytes = random_bytes(rng, Self::max_size());
-        Self::from(bytes)
+        let size = rng.rand_u32() % Self::max_size();
+        let mut buf = Vec::with_capacity(size as usize);
+        for _ in 0..size {
+            buf.push(rng.rand_u8());
+        }
+        Self::from(buf)
+    }
+}
+
+impl<const K: usize> Random for UnboundedVecN<K> {
+    fn random(rng: &mut Rng) -> Self {
+        let size = rng.rand_u32() % Self::max_size();
+        let mut buf = Vec::with_capacity(size as usize);
+        for _ in 0..size {
+            buf.push(rng.rand_u8());
+        }
+        Self::from(buf)
     }
 }
 
