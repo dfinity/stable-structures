d5a4464b76c1f037f179261ed3437670318f2d01
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-05-27 15:22:21 +0200

    .

--- benchmarks/src/common.rs
+++ benchmarks/src/common.rs
@@ -19,7 +19,7 @@ impl<const K: usize> Random for Blob<K> {
     }
 }
 
-impl<const K: usize> Random for BoundedVecN<K> {
+impl<const K: usize> Random for UnboundedVecN<K> {
     fn random(rng: &mut Rng) -> Self {
         let size = rng.rand_u32() % Self::max_size();
         let mut buf = Vec::with_capacity(size as usize);
@@ -30,7 +30,7 @@ impl<const K: usize> Random for BoundedVecN<K> {
     }
 }
 
-impl<const K: usize> Random for UnboundedVecN<K> {
+impl<const K: usize> Random for BoundedVecN<K> {
     fn random(rng: &mut Rng) -> Self {
         let size = rng.rand_u32() % Self::max_size();
         let mut buf = Vec::with_capacity(size as usize);
