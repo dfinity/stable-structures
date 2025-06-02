4d8c480eac34eb8d485afc6a13ea2b7ab9c4cff5
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-06-02 11:30:10 +0200

    canbench main

--- scripts/ci_run_benchmark.sh
+++ scripts/ci_run_benchmark.sh
@@ -24,7 +24,8 @@ MAIN_BRANCH_RESULTS_FILE="$MAIN_BRANCH_DIR/$CANBENCH_RESULTS_FILE"
 CANBENCH_RESULTS_CSV_FILE="/tmp/canbench_results_${CANBENCH_JOB_NAME}.csv"
 
 # Install canbench.
-cargo install --version 0.1.15 canbench
+#cargo install --version 0.1.15 canbench
+cargo install --git https://github.com/dfinity/canbench canbench
 
 # Verify that the canbench results file exists.
 if [ ! -f "$CANBENCH_RESULTS_FILE" ]; then
