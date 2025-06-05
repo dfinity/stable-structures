d1d979efb69bfb10fa7af6e00a258a2739b853aa
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-06-05 12:45:19 +0200

    chore: bump canbench version to v0.1.16

--- scripts/ci_run_benchmark.sh
+++ scripts/ci_run_benchmark.sh
@@ -24,7 +24,7 @@ MAIN_BRANCH_RESULTS_FILE="$MAIN_BRANCH_DIR/$CANBENCH_RESULTS_FILE"
 CANBENCH_RESULTS_CSV_FILE="/tmp/canbench_results_${CANBENCH_JOB_NAME}.csv"
 
 # Install canbench.
-cargo install --version 0.1.15 --locked canbench
+cargo install --version 0.1.16 --locked canbench
 
 # Verify that the canbench results file exists.
 if [ ! -f "$CANBENCH_RESULTS_FILE" ]; then
